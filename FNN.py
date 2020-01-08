42b75fce4b523cef990552ab1b20b3fa1a0c3135
Sharkeon <nick@xl5.de>
2020-01-08 23:13:51 +0100

    Added and implemented: _train_calcWeightGradients(), _mapMatrix(), _train_calcErrors(), _dActFuncPreActFunc(), _dsigmoidPreSigmoid(): it is now possible to calculate the gradients

--- FNN.py
+++ FNN.py
@@ -1,15 +1,16 @@
 import numpy as np
 #Activation: RELU
 class FNN:
-    def __init__(self,neuronsInput,layersHidden,neuronsHidden,neuronsOutput,activationFunction):
+    def __init__(self,neuronsInput,layersHidden,neuronsHidden,neuronsOutput,activationFunction,learningRate):
         #Set object vars
         self.nIS = neuronsInput
         self.lHS = layersHidden
         self.nHS = neuronsHidden
         self.nOS = neuronsOutput
+        self.lr = learningRate
         self.tNL = 1+self.lHS+1                                         #Number of total layers
         self.vWS = self.tNL-1                                           #Number of weight matrixs
-        self.bias = np.zeros(shape=(self.vWS))                          #np.random.rand(self.vWS)
+        self.bias = np.random.rand(self.vWS)#np.zeros(shape=(self.vWS))
         self.actFuncName = activationFunction
         self.aLLP = []                                                  #"All Layers Last Prediction" stores the layers and neurons from last prediction for the training (backpropagation)            
         #Initialise input and output layers
@@ -27,9 +28,16 @@ class FNN:
     def _sigmoid(self,x):                                               #Sigmoid - activation function
         return 1/(1+np.exp(-x))                                         #__Applay sigmoid function to x and return it: sigmoid(x) = 1/1(1+exp(-x))
 
+    def _dsigmoidPreSigmoid(self,x):
+        return x*(1-x)                                                  #dsigmoid = sigmoid(x)*(1-sigmoid(x)), but sigmoid is already apllied on x
+
     def _actFunc(self,x):                                               #Define activation function
         if self.actFuncName == "sigmoid":
             return self._sigmoid(x)
+
+    def _dActFuncPreActFunc(self,x):                           #Define derivative of activation function, but activation function is already apllied to x
+        if self.actFuncName == "sigmoid":
+            return self._dsigmoidPreSigmoid(x)
 
     def _calcNextLayer(self,layer1,weights,bias):                       #Calculate next Layer with layer bevor and weights and bias
         self.layer2 = np.matmul(weights,layer1)+bias                    #__Calculate next layer: Layer2_vector = (Weights_matrix * Layer1_vector) + bias
@@ -43,17 +51,15 @@ class FNN:
         self.allLayers = []                                             #__Create array for all layers - collection
         self.allLayers.append(self.layerInput)                          #__Add InputLayer to collection
         for x in self.layerHidden:                                      #__Foreach element in Hidden Layers (HiddenLayer_1,...,HiddenLayer_n) 
-            self.allLayers.append(x)                                    #__Add each HiddenLayer to collection
+            self.allLayers.append(x)                                    #____Add each HiddenLayer to collection
         self.allLayers.append(self.layerOutput)                         #__Add OutputLayer to collection
         for i in range(self.tNL-1):                                     #__Iterate over all Layers(collection)
-            self.allLayers[i+1] = self._calcNextLayer(self.allLayers[i],self.matrixWeights[i],self.bias[i])#__Calculate next Layer and set it in the collection (all Layers)
-        self.aLLP = self.allLayers                        #__Spacer for the collection, needed for the training to get the last collection neurons for backpropagation
+            self.allLayers[i+1] = self._calcNextLayer(self.allLayers[i],self.matrixWeights[i],self.bias[i])#____Calculate next Layer and set it in the collection (all Layers)
+        self.aLLP = self.allLayers                                      #__Spacer for the collection, needed for the training to get the last collection neurons for backpropagation
         return self.allLayers[self.tNL-1]                               #__Return OutputLayer (last layer of collection)
 
-    def train(self,inputData,expectedResult):
-        self.errors = np.zeros(shape=(len(expectedResult),1))           #__Create error_vector
-        self.prediction = self.predict(inputData)                       #__Calculate prediction
-        self.allLayers = self.aLLP                                      #__Load spacer for collection (all neurons from last prediction) to get the neurons/layers 
+    def _train_calcErrors(self,expectedResult,prediction):              #Calculate all error_vectors foreach layer
+        self.errors = np.zeros(shape=(len(expectedResult),1))           #__Create error_vector aka error_vector_output
         for i in range(len(self.errors)):                               #__Calculate error foreach prediction and add to error_vector
             self.errors[i][0] = expectedResult[i] - self.prediction[i]
         self.allLayersError = []                                        #__Create array (aka error_collection) for error_vectors foreach Layer, so every Layer has a error_vector
@@ -63,9 +69,34 @@ class FNN:
         self.allLayersError.append(np.zeros(shape=(self.nOS,1)))
         self.allLayersError[len(self.allLayersError)-1] = self.errors   #__Set last error-layer as calculated error_vector (output-error)
         for i in range(self.tNL-1):                                     #__Calcluate each error_vector from last layer to first (output-->hidden-->input)
-            x = self.tNL-1 - i                                          #__Because from back to front
+            x = self.tNL-1 - i                                          #____Because from back to front
             self.allLayersError[x-1] = np.matmul(self.matrixWeights[x-1].T, self.allLayersError[x])
         return self.allLayersError
+
+    def _mapMatrix(self,matrix,function):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] = function(matrix[i][j])
+        return matrix
+
+    def _train_calcWeightGradients(self,allLayers,allLayersError):      #Calculate all Weight Gradient Matrixs foreach Weight Matrix
+        self.matrixWeightsGradients = []                                #__Create Array for the Weight Gradients Matrixs
+        for i in range(self.vWS):
+            self.matrixWeightsGradients.append(\
+                       self.lr * \
+                       allLayersError[i+1] * \
+                       self._mapMatrix(allLayers[i+1],self._dActFuncPreActFunc) * \
+                       allLayers[i].T)
+        return self.matrixWeightsGradients
+
+
+
+    def train(self,inputData,expectedResult):
+        self.prediction = self.predict(inputData)                       #__Calculate prediction
+        self.allLayers = self.aLLP                                      #__Load spacer for collection (all neurons from last prediction) to get the neurons/layers 
+        self.allLayersError = self._train_calcErrors(expectedResult,self.prediction)
+        self.matrixWeightsGradients = self._train_calcWeightGradients(self.allLayers,self.allLayersError)
+        return self.matrixWeightsGradients
 
    
 
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from FNN import FNN
 
 
 if __name__ == "__main__":
-    myFNN = FNN(neuronsInput=4,layersHidden=1,neuronsHidden=4,neuronsOutput=4,activationFunction="sigmoid")
+    myFNN = FNN(neuronsInput=4,layersHidden=1,neuronsHidden=4,neuronsOutput=4,activationFunction="sigmoid",learningRate=0.2)
     print("---------------------------------------------------------")
     print("Test Predict")
     #print(myFNN.predict([1,0,0,0,0,0,0,0,0]))
     print("Input [1,0]: ")
     print(myFNN.predict([1,0,1,1]))
-    print("Error [1,1,0,1]-->1: ")
+    print("Gradients [1,1,0,1]-->1: ")
     print(myFNN.train([1,0,1,1],[1,1,0,1]))
     print("---------------------------------------------------------")
